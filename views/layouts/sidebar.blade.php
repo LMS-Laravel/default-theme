f61b63fbb080b75d85ae5097e284166292b515a2
Angel Kurten <angelkurten@hotmail.com>
2015-11-06 02:00:41 -0500

    Fix Sidebar

--- views/layouts/sidebar.blade.php
+++ views/layouts/sidebar.blade.php
@@ -19,10 +19,9 @@
                 </div>
                 <div class="sidebar-user-name">{{{ Auth::user()->full_name }}}</div>
                 <div class="sidebar-user-links">
-                    <a href="{{ route('learning.user.profile', \Auth::user()->slug) }}" data-toggle="tooltip" data-placement="bottom" title="Perfil"><i class="gi gi-user"></i></a>
-                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Mensajes"><i class="gi gi-envelope"></i></a>
-                    <a href="#modal-user-settings" data-toggle="modal" class="enable-tooltip" data-placement="bottom" title="Ajustes"><i class="gi gi-cogwheel"></i></a>
-                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Salir"><i class="gi gi-exit"></i></a>
+                    <a href="{{ route('learning.user.profile', \Auth::user()->slug) }}" data-toggle="tooltip" data-placement="bottom" title="@trans('dashboard::menus.sidebar.profile')"><i class="gi gi-user"></i></a>
+                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="@trans('dashboard::menus.sidebar.inbox')"><i class="gi gi-envelope"></i></a>
+                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="@trans('dashboard::menus.sidebar.exit')"><i class="gi gi-exit"></i></a>
                 </div>
             </div>
             <!-- END User Info -->
@@ -30,18 +29,15 @@
             <!-- Sidebar Navigation -->
             <ul class="sidebar-nav">
                 <li>
-                    <a href="#"><i class="gi gi-stopwatch sidebar-nav-icon"></i>Cursos</a>
+                    <a href="@route('learning.course.index')"><i class="gi gi-stopwatch sidebar-nav-icon"></i>@trans('dashboard::menus.sidebar.courses')</a>
                 </li>
                 <li>
-                    <a href="#"><i class="gi gi-share_alt sidebar-nav-icon"></i>Social</a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-play sidebar-nav-icon"></i>En vivo</a>
+                    <a href="#"><i class="gi gi-share_alt sidebar-nav-icon"></i>@trans('dashboard::menus.sidebar.social')</a>
                 </li>
                 <li class="sidebar-header">
                     <span class="sidebar-header-options clearfix">
-                    <a href="javascript:void(0)" data-toggle="tooltip" title="Solo quienes aprueben un examen o quiz ganaran puntos"><i class="gi gi-lightbulb"></i></a></span>
-                    <span class="sidebar-header-title">Ranking</span>
+                    <a href="javascript:void(0)" data-toggle="tooltip" title="@trans('dashboard::menus.sidebar.submenu.ranking.help')"><i class="gi gi-lightbulb"></i></a></span>
+                    <span class="sidebar-header-title">@trans('dashboard::menus.sidebar.submenu.ranking.title')</span>
                 </li>
                 <li>
                     <a href="#"><i class="gi gi-cup sidebar-nav-icon"></i>Global</a>
